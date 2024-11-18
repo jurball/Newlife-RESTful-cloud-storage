5641717b3401331fd26f02a3f9c713120477083f
kabinetleonova <kabinetleonova@bk.ru>
2024-11-18 13:29:50 +0300

    add CustomUser Model changed base User model in settings.py add register for CustomUser

--- metanit/database/admin.py
+++ metanit/database/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from database.models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ("Основная информация", {'fields': ('email', 'password')}),
+        ('Личная информация', {'fields': ('first_name', 'last_name')}),
+        ('Разрешения', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Важные даты', {'fields': ('last_login', 'date_joined')}),
+    )
+
--- metanit/metanit/settings.py
+++ metanit/metanit/settings.py
@@ -87,6 +87,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'database.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
